e13cf332139acb64865abc7cc208a7dbdd170cee
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-02-10 13:31:04 +0530

    Login Page ui done

--- Booking_System/src/App.jsx
+++ Booking_System/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import SignInSide from "./components/SignIn/SignInSlide";
+import { LoginForm } from "./components/login-form";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <SignInSide />
+      <LoginForm />
     </>
   );
 }
